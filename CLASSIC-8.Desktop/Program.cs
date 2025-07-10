3610234d5ea63d9f6e3f39c1cba05086bbbb8d72
Andrew Nelson <evildarkarchon@gmail.com>
2025-07-09 20:28:15 -0700

    Starting Over

--- CLASSIC-8.Desktop/Program.cs
+++ CLASSIC-8.Desktop/Program.cs
@@ -1,28 +1,24 @@
 ﻿using System;
+
 using Avalonia;
 using Avalonia.ReactiveUI;
 
 namespace CLASSIC_8.Desktop;
 
-internal class Program
+class Program
 {
     // Initialization code. Don't use any Avalonia, third-party APIs or any
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
     // yet and stuff might break.
     [STAThread]
-    public static void Main(string[] args)
-    {
-        BuildAvaloniaApp()
-            .StartWithClassicDesktopLifetime(args);
-    }
+    public static void Main(string[] args) => BuildAvaloniaApp()
+        .StartWithClassicDesktopLifetime(args);
 
     // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()
-    {
-        return AppBuilder.Configure<App>()
+        => AppBuilder.Configure<App>()
             .UsePlatformDetect()
             .WithInterFont()
             .LogToTrace()
             .UseReactiveUI();
-    }
 }
--- CLASSIC-8/App.axaml.cs
+++ CLASSIC-8/App.axaml.cs
@@ -1,6 +1,7 @@
 ﻿using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+
 using CLASSIC_8.ViewModels;
 using CLASSIC_8.Views;
 
@@ -16,15 +17,19 @@ public partial class App : Application
     public override void OnFrameworkInitializationCompleted()
     {
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+        {
             desktop.MainWindow = new MainWindow
             {
                 DataContext = new MainViewModel()
             };
+        }
         else if (ApplicationLifetime is ISingleViewApplicationLifetime singleViewPlatform)
+        {
             singleViewPlatform.MainView = new MainView
             {
                 DataContext = new MainViewModel()
             };
+        }
 
         base.OnFrameworkInitializationCompleted();
     }
